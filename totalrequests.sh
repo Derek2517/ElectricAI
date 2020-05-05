7a3f4589b422bed2be0d8ca31b554b082cf145fe
Derek Choate <dchoate42@gmail.com>
2020-05-05 09:15:35 -0500

    Update totalrequests.sh

--- totalrequests.sh
+++ totalrequests.sh
@@ -6,7 +6,7 @@ do
 #Look for filename... in this example if the file is /var/log/requsts.log then proceed; Change Filename to suit correct naming scheme.    
 	if [ "${file}" == "/var/log/requests.log" ]
 	then
-#Counting requests by first greping all lines that begin with '/' then using awk to sort through what is left and grab the URL and request count, using sed to flip output to left to right.     
+#Counting requests by first using grep to remove all lines that begin with '/' then using awk to sort through what is left and grab the URL and request count, using sed to flip output to left to right.     
 		countRequests=$(grep -v '^/' ${file}| awk 'NF''{print $1}' | sort | sort -nr | uniq -c | sed -E 's/\s*(\S+)\s+(\S+)/\2 \1/')
 #Output Results in STDOUT		
         echo "${countRequests}"
